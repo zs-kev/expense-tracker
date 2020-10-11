c1426152d363bc70edfd8789beb6db6759da8390
Kevin Simon <keviniansimon@gmail.com>
2020-10-11 13:55:29 +0200

    Initial commit

---
+++ assets/js/addExpense.js
@@ -0,0 +1,7 @@
+const addExpense = (event) => {
+	const expenseName = document.querySelector('#expenseName');
+	const expenseDate = document.querySelector('#expenseDate');
+	const expenseAmount = document.querySelector('#expenseAmount');
+	
+	console.log(expenseName, expenseDate, expenseAmount);
+}
---
+++ assets/js/app.js
@@ -0,0 +1,5 @@
+const expenseSubmit = document.getElementsByClassName('expenseSubmit');
+
+console.log(expenseSubmit);
+
+expenseSubmit.addEventListener('click', addExpense);
